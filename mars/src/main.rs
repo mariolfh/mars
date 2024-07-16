192160a82617920670eb7d3a06aeae7e1e083b32
Overlord <64396959+mariolfh@users.noreply.github.com>
2024-07-15 23:47:28 -0400

    Added lowercase function
    Following up, i added the lowercase function which takes a string and makes it lowercase

--- mars/src/main.rs
+++ mars/src/main.rs
@@ -13,6 +13,9 @@ enum Commands {
     Mars,
     Uppercase {
         value: String
+    },
+    Lowercase {
+        value: String
     }
 }
 
@@ -22,11 +25,17 @@ fn main() {
     match cli.com {
         Commands::Welcome => println!("Welcome to Mars!"),
         Commands::Mars => println!("“Mars is there, waiting to be reached.” -Buzz Aldrin, American pilot, and astronaut"),
-        Commands::Uppercase{value} => uppercase(value)
+        Commands::Uppercase{value} => uppercase(value),
+        Commands::Lowercase{value} => lowercase(value)  
     }
 }
 
 fn uppercase(value: String) {
     let result = value.to_uppercase();
+    println!("{}", result);
+}
+
+fn lowercase(value: String) {
+    let result = value.to_lowercase();
     println!("{}", result);
 }
