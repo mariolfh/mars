49cc46feaf6a7ccd78454ca3ebb16529155ab540
Overlord <64396959+mariolfh@users.noreply.github.com>
2025-10-13 03:02:27 -0400

    Created functions related to paste and it's local file
    In a similar way I previously did, the paste command now has it's file operations, keeping more or less the same syntax, making them very similar. Some further adjustments might be needed, but it's a good step in the right direction. Not related, but Happy Jiminie Day!

--- mars/src/fileops.rs
+++ mars/src/fileops.rs
@@ -35,3 +35,36 @@ pub fn get_copier() -> io::Result<String> {
     file.read_to_string(&mut content);
     Ok(content)
 }
+
+/// Paste related file operations
+pub fn get_ppath() -> PathBuf{
+    let mut ppath = env::temp_dir();
+    ppath.push("mars-paste.txt");
+    ppath
+}
+
+pub fn ppath_exists() -> io::Result<()> {
+    let path = get_ppath();
+    if !path.exists() {
+        File::create(&path)?;
+    }
+    Ok(())
+}
+
+pub fn save_pastier(value: &str) -> io::Result<()> {
+    let path = get_ppath();
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open(&path)?;
+    file.write_all(value.as_bytes())?;
+    Ok(())
+}
+
+pub fn get_pastier() -> io::Result<String> {
+    let path = get_ppath();
+    let mut file = File::open(&path)?;
+    let mut content = String::new();
+    file.read_to_string(&mut content);
+    Ok(content)
+}
